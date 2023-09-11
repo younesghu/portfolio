9f61e2861869500e2c10cb9a447576452a8b83c6
Younes Guigaho <younesghu44@gmail.com>
2023-09-11 20:49:39 +0100

    update 0.2

--- mail.php
+++ mail.php
@@ -1,34 +1,25 @@
 <?php
-
-    $username = $_POST["name"];
-    $useremail = $_POST["email"];
-    $usermessage = $_POST["message"];
-    $subject = "test";
-
-    $data = "From: ".$username."<".$useremail.">\r\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
     $to = "younesghu44@gmail.com";
 
-    mail($to,$subject,$message,$data)
-    or die("Error!");
+    $subject = "Contact Form Submission from $name";
 
-    echo '
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Thank you</title>
-        <link rel="stylesheet" href="assets/css/meyawo.css">
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message";
 
-    </head>
-    <body>
-        <div class="thankyou">
-            <h6>Thank you for contacting me.</h6>
-            <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        </div>
-    </body>
-    </html>
-    
-    ';
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "Thank you for contacting us. We will get back to you soon!";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+} else {
+    echo "Access denied.";
+}
 ?>
