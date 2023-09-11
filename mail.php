2f21f2ae3e079baeb967e5003d5aea216f92f619
Younes Guigaho <younesghu44@gmail.com>
2023-09-11 22:44:05 +0100

    php uploading

--- mail.php
+++ mail.php
@@ -1,6 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
@@ -8,19 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "younesghu44@gmail.com";
 
     $subject = "Contact Form Submission from $name";
-
-    $email_message = "Name: $name\n";
-    $email_message .= "Email: $email\n";
-    $email_message .= "Message:\n$message";
+    $txt = "Name: \n" . $name . "Email: \n" . $email . "Message:\n" . $message;
 
     $headers = "From: $email";
 
-    if (mail($to, $subject, $email_message, $headers)) {
-        echo "Thank you for contacting us. We will get back to you soon!";
-    } else {
-        echo "Oops! Something went wrong. Please try again later.";
+    if ($email!=NULL) {
+        mail($to, $subject, $txt, $headers);
     }
-} else {
-    echo "Access denied.";
-}
+    header("Location:thankyou.html");
+
 ?>
